6d8277a316004651eb3a8d1b42c123c87723823e
Dmitry828 <git@github.com:Dmitry828/tmdb.git>
2022-09-19 21:46:43 +0300

    1 commit

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,9 +1,9 @@
-import { Wrapper } from "./styled";
+import { Wrapper } from './styled';
 
 export const Auth = () => {
   return (
     <Wrapper>
-
+      <div>22222222222</div>
     </Wrapper>
   );
 };
